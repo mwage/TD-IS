9e0af0e1b39b3b9c5cf90a2e7a0436efaef57bc4
Michael Wagner <mwagner@sba-research.org>
2024-06-21 22:14:58 +0200

    graph and td generation with sage

--- graph_gen/helper.py
+++ graph_gen/helper.py
@@ -1,12 +1,54 @@
-# Library imports
-import pandas as pd
-import numpy as np
-import math
-import matplotlib.pyplot as plt
-from matplotlib.ticker import LogFormatterSciNotation
-from itertools import groupby
-from math import prod
+from sage.graphs.graph import Graph
+from sage import graphs
+from sage.graphs.graph_decompositions.tree_decomposition import TreelengthConnected
+from random import *
 
-class Graph:
-    def __init__(self, vertices):
-    self.vertices = vertices
+def create_rand_graph(num_vertices, density, upper_weight):
+    while True:
+        graph = Graph(num_vertices)
+        for v in range(0, num_vertices):
+            w = randrange(0, num_vertices-1)
+            if w >= v:
+                w += 1
+            graph.add_edge(v,w)
+
+        while graph.density() < density:
+            v = randrange(0, num_vertices)
+            w = randrange(0, num_vertices-1)
+            if w >= v:
+                w += 1
+            graph.add_edge(v,w)
+
+        if graph.is_connected():
+            weights = [randint(1,upper_weight) for _ in range(0,num_vertices)]
+            return (graph, weights)
+
+def get_graph_str(graph, weights):
+    g_str = "\n".join([f"{v},,{weights[v]}" for v in graph.vertices()])
+    g_str += "\n"
+    g_str += "\n".join([f"{edge[0]},{edge[1]}" for edge in graph.edges()])
+    return g_str
+
+def get_td_str(graph):
+    td = TreelengthConnected(graph, certificate=True).get_tree_decomposition()
+    td_str = ""
+    vertices = {}
+    for (i, v) in enumerate(td.vertices()):
+        vertices[v] = i
+        v_names = ";".join([str(x) for x in v])
+        td_str += f"N{i},,{v_names}\n"
+    td_str += "\n".join([f"N{vertices[edge[0]]},N{vertices[edge[1]]}," for edge in td.edges()])
+    return td_str
+
+def create_instance(num_vertices, density, upper_weight, name):
+    (g, weights) = create_rand_graph(num_vertices, density, upper_weight)
+    g_str = get_graph_str(g, weights)
+    td_str = get_td_str(g)
+
+    print(g_str)
+    print(td_str)
+
+    with open(f"../TD-IS/instances/{name}G.csv", "w") as text_file:
+        text_file.write(g_str)
+    with open(f"../TD-IS/instances/{name}TD.csv", "w") as text_file:
+        text_file.write(td_str)
